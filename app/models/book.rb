d455643c8a4e1849469db2ee82d4738117c29c79
Sébastien Carceles <sebastien.carceles@gmail.com>
2018-05-16 19:02:59 +0200

    add validations and specs

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,4 @@
 class Book < ApplicationRecord
+  validates :title, :author, presence: true
+  validates :pages_count, numericality: { only_integer: true, greater_than: 0 }, allow_blank: true
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -10,4 +10,28 @@ RSpec.describe Book, type: :model do
   it "can be saved" do
     expect(book.save).to eq true
   end
+
+  it "is invalid without title" do
+    book.title = nil
+    expect(book.valid?).to eq false
+    expect(book.errors.messages).to have_key(:title)
+  end
+
+  it "is invalid without author" do
+    book.author = nil
+    expect(book.valid?).to eq false
+    expect(book.errors.messages).to have_key(:author)
+  end
+
+  it "is invalid with a negative pages count" do
+    book.pages_count = -1
+    expect(book.valid?).to eq false
+    expect(book.errors.messages).to have_key(:pages_count)
+  end
+
+  it "is invalid with a 0 pages count" do
+    book.pages_count = 0
+    expect(book.valid?).to eq false
+    expect(book.errors.messages).to have_key(:pages_count)
+  end
 end
