d501beb6bcfa9c4b67e749f7c9b045b012818713
Sébastien Carceles <sebastien.carceles@gmail.com>
2018-05-18 09:47:03 +0200

    edit and update actions

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,12 +11,25 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
   def create
     @book = Book.new(params.require(:book).permit(:title, :author, :description, :pages_count, :published_at))
     if @book.save
       redirect_to books_path
     else
       render :new
+    end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(params.require(:book).permit(:title, :author, :description, :pages_count, :published_at))
+      redirect_to books_path
+    else
+      render :edit
     end
   end
 end
