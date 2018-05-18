908bfad648af6c0896211c19747061eb04eb8511
Sébastien Carceles <sebastien.carceles@gmail.com>
2018-05-18 10:02:26 +0200

    DRY refactoring

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,11 @@
 class BooksController < ApplicationController
+  before_action :find_book, only: [:show, :edit, :update]
+
   def index
     @books = Book.all
   end
 
   def show
-    @book = Book.find(params[:id])
   end
 
   def new
@@ -12,11 +13,10 @@ class BooksController < ApplicationController
   end
 
   def edit
-    @book = Book.find(params[:id])
   end
 
   def create
-    @book = Book.new(params.require(:book).permit(:title, :author, :description, :pages_count, :published_at))
+    @book = Book.new(book_params)
     if @book.save
       redirect_to books_path
     else
@@ -25,11 +25,20 @@ class BooksController < ApplicationController
   end
 
   def update
-    @book = Book.find(params[:id])
-    if @book.update(params.require(:book).permit(:title, :author, :description, :pages_count, :published_at))
+    if @book.update(book_params)
       redirect_to books_path
     else
       render :edit
     end
   end
+
+  private
+
+    def book_params
+      params.require(:book).permit(:title, :author, :description, :pages_count, :published_at)
+    end
+
+    def find_book
+      @book = Book.find(params[:id])
+    end
 end
