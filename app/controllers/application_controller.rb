806bd89f579f4c77f1e1e6a1dc9776820596fe63
Sébastien Carceles <sebastien.carceles@gmail.com>
2018-05-26 10:10:09 +0200

    set the locale from param, session or default

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  private
+
+    def set_locale
+      locale = params[:locale] || session[:locale] || I18n.default_locale
+      I18n.locale = locale
+      session[:locale] = locale
+    end
 end
