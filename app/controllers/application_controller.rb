784235d18053f48bc761f001a55b5ced8156a44d
Sébastien Carceles <sebastien.carceles@gmail.com>
2018-05-26 14:49:06 +0200

    add the locale as default url options

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
 
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
   private
 
     def set_locale
-      locale = params[:locale] || session[:locale] || I18n.default_locale
-      I18n.locale = locale
-      session[:locale] = locale
+      I18n.locale = params[:locale] || I18n.default_locale
+      # cookies[:locale] = { value: locale, expires: 30.days }
     end
 end
