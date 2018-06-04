b1646fc1988e94ad7bac604082fb26ba46b2de4c
Sébastien Carceles <sebastien.carceles@gmail.com>
2018-06-04 12:25:39 +0200

    add the sidekiq monitoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
+
+
   root "home#index"
   get "home", to: "home#index"
   resources :books, only: [:index, :show, :new, :edit, :create, :update, :destroy], param: :identifier
