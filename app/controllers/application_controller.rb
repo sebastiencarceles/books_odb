0d0135b02f58b72963cefaf2e65e4ff7d9f840df
Sébastien Carceles <sebastien.carceles@gmail.com>
2018-06-05 11:00:35 +0200

    edit the first_name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
 
   def default_url_options
     { locale: I18n.locale }
   end
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:first_name])
+    end
 
   private
 
