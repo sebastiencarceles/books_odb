0e8ccd997f17ccc9808bc2f9c2f8a8c40a85e317
Sébastien Carceles <sebastien.carceles@gmail.com>
2018-06-05 11:25:36 +0200

    use the safe navigation operator instead

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -2,7 +2,7 @@ module BooksHelper
   def summary(book)
     summary = []
     summary << truncate(book.description, length: 50, separator: " ") if book.description.present?
-    summary << t("helpers.books.written_by", editor_name: book.user.first_name) if book.user
+    summary << t("helpers.books.written_by", editor_name: book.user.first_name) if book.user&.first_name&.present?
     summary.join("... ")
   end
 end
